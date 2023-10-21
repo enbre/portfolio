8313ff27d0fdd7b45e60bcf38ffd3e805dcb299c
enbre <noahbrezel@gmail.com>
2023-10-20 17:17:45 -0700

    adds new skill logos

--- js/main.js
+++ js/main.js
@@ -1,34 +1,13 @@
 console.log("sanity check")
 
 
-let mediaQuery = window.matchMedia('(max-width: 700px)')
- console.log(mediaQuery.matches)
-// if (mediaQuery.matches) {
-//    // alert("Media query matched!")
-// }
-
 document.addEventListener('DOMContentLoaded', function () {
    var elems = document.querySelectorAll('.dropdown-trigger');
+   console.log(elems)
    var instances = M.Dropdown.init(elems, {
       direction: 'bottom',
       coverTrigger: false,
       hover: true
 
-      // if(mediaQuery.matches){
-      //    hover: false,
-      // }
-
 });
 });
-
-// window.onscroll = function() {stickynav()}
-
-// const navbar = document.getElementById("navbar");
-
-// const stickyHeight = navbar.offsetTop;
-
-// const stickynav =()=>{
-//    if(window.pageYOffset >= stickyHeight){
-//       navbar.classList.add("sticky")
-//    } else navbar.classList.remove("sticky")
-// }
